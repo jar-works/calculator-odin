f19dcccb77fb90210c6eaf065b1e221b0836197c
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2025-01-02 17:38:34 -0500

    feat: Apply event listening for the calc buttons

--- script.js
+++ script.js
@@ -44,12 +44,69 @@ function updateDisplay() {
     console.log("updating display");
     let displayStr = "";
     for (let i = 0; i < currentOperations.length; i++) {
-        if (typeof(currentOperations[i]) === "number") {
+        if (typeof(currentOperations[i]) === "number" || currentOperations[i] === ".") {
             displayStr = displayStr.concat(currentOperations[i]);
         } else {
-            displayStr = displayStr.concat(" " + currentOperations[i]);
+            displayStr = displayStr.concat(" " + currentOperations[i] + " ");
         }
     }
     displayText.textContent = displayStr;
 }
 
+const btnsContainer = document.querySelector(".btns-container");
+btnsContainer.addEventListener("click", (event) => {
+    let target = event.target;
+
+    switch(target.textContent) {
+        case "1":
+            currentOperations.push(1);
+            break;
+        case "2":
+            currentOperations.push(2);
+            break;
+        case "3":
+            currentOperations.push(3);
+            break;
+        case "4":
+            currentOperations.push(4);
+            break;
+        case "5":
+            currentOperations.push(5);
+            break;
+        case "6":
+            currentOperations.push(6);
+            break;
+        case "7":
+            currentOperations.push(7);
+            break;
+        case "8":
+            currentOperations.push(8);
+            break;
+        case "9":
+            currentOperations.push(9);
+            break;
+        case "0":
+            currentOperations.push(0);
+            break;
+        case "+":
+            currentOperations.push("+");
+            break;
+        case "-":
+            currentOperations.push("-");
+            break;
+        case "x":
+            currentOperations.push("x");
+            break;
+        case "/":
+            currentOperations.push("/");
+            break;
+        case ".":
+            currentOperations.push(".");
+            break;
+        case "=":
+            // currentOperations.push("=");
+            break;
+    }
+
+    updateDisplay();
+})
