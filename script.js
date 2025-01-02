08e19ebdadb1c4b29f0ea307fd3ee6d19b980797
Jose Ruiz <38330657+jruiiz@users.noreply.github.com>
2025-01-02 16:46:39 -0500

    feat: Implement operator function

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function multiply(a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+// Operation being either a, s, m, d
+function operator(a, b, operation) {
+    let result;
+
+    switch(operation) {
+        case 'a':
+            result = add(a, b);
+            break;
+        case 's':
+            result = subtract(a, b);
+            break;
+        case 'm':
+            result = multiply(a, b);
+            break;
+        case 'd':
+            result = divide(a, b);
+            break;
+    }
+
+    return result;
+}
